acfa6f169ecabb257d92fa990cb1de620e21f757
kunal <ks1140175@gmail.com>
2026-04-18 01:40:38 +0530

    sanity updates

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,15 +1,15 @@
 // lib/sanity.ts
 import { createClient } from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url';
+import createImageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2024-01-01',
-  useCdn: false,
+  useCdn: true,
 });
 
-const builder = imageUrlBuilder(client);
+const builder = createImageUrlBuilder(client);
 
 export function urlFor(source: any) {
   return builder.image(source);
