52268abb0a4bcebc9d900425e17248f15edc3329
kunal <ks1140175@gmail.com>
2026-05-26 18:12:25 +0530

    new blog

--- app/blog/kajabi-to-gohighlevel-migration/client.tsx
+++ app/blog/kajabi-to-gohighlevel-migration/client.tsx
@@ -217,7 +217,7 @@ export default function KajabiToGHLMigrationClient() {
                                           <img
                                                 src="/web-app-manifest-192x192.png"
                                                 alt="GHL Scale Up"
-                                                className="w-8 h-8 object-cover"
+                                                className="w-full h-full object-cover"
                                           />
                                     </div>
                                     <div>
