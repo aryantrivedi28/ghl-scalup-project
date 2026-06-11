5aa423095b2a4dc950ba481c5bbd688bae189239
kunal <ks1140175@gmail.com>
2026-06-12 00:40:55 +0530

    new blog updated

--- app/blog/gohighlevel-saas-mode-setup/client.tsx
+++ app/blog/gohighlevel-saas-mode-setup/client.tsx
@@ -225,7 +225,7 @@ export default function GoHighLevelSaaSModeSetupClient() {
                                           <img
                                                 src="/web-app-manifest-192x192.png"
                                                 alt="GHL Scale Up"
-                                                className="w-8 h-8 object-cover"
+                                                className="w-full h-full object-cover"
                                           />
                                     </div>
                                     <div>
