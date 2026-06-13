bc3dfe047ca39cb5a52cc5d408fda25eff1bd368
kunal <ks1140175@gmail.com>
2026-06-13 18:24:36 +0530

    new blog added

--- app/blog/gohighlevel-migration-checklist/client.tsx
+++ app/blog/gohighlevel-migration-checklist/client.tsx
@@ -315,7 +315,7 @@ export default function GHLMigrationChecklistClient() {
               <img
                 src="/web-app-manifest-192x192.png"
                 alt="GHL Scale Up"
-                className="w-8 h-8 object-cover"
+                className="w-full h-full object-cover"
               />
             </div>
             <div>
