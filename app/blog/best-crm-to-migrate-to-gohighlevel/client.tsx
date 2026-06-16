44e94b495ac5483038805a56f87d4e9e12d23f5f
kunal <ks1140175@gmail.com>
2026-06-17 00:38:35 +0530

    new blog added

--- app/blog/best-crm-to-migrate-to-gohighlevel/client.tsx
+++ app/blog/best-crm-to-migrate-to-gohighlevel/client.tsx
@@ -242,7 +242,7 @@ export default function BestCRMToMigrateToGHLClient() {
               <img
                 src="/web-app-manifest-192x192.png"
                 alt="GHL Scale Up"
-                className="w-8 h-8 object-cover"
+                className="w-full h-full object-cover"
               />
             </div>
             <div>
