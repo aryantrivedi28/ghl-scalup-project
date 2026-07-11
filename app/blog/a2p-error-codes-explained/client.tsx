bf46abc0ae1d95c902a71af8d65cf45558b8bda6
kunal <ks1140175@gmail.com>
2026-07-11 15:23:16 +0530

    new blog

--- app/blog/a2p-error-codes-explained/client.tsx
+++ app/blog/a2p-error-codes-explained/client.tsx
@@ -144,7 +144,7 @@ export default function A2PErrorCodesExplainedClient() {
 
           {/* Author */}
           <div className="flex items-center gap-3 mb-6">
-            <div className="w-7 h-7 overflow-hidden bg-white flex items-center justify-center rounded-full">
+            <div className="w-7 h-7 overflow-hidden bg-white flex items-center justify-center">
               <img
                 src="/web-app-manifest-192x192.png"
                 alt="GHL Scale Up"
